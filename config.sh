812e5f7ddf01aef158d02d0cf081b9b0723bf960
zys864 <zyspage@gmail.com>
2022-05-22 00:11:35 +0800

    edit

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@ cp /etc/nix/nix.conf /etc/nix/nix.conf1
 rm /etc/nix/nix.conf
 mv /etc/nix/nix.conf1 /etc/nix/nix.conf
 chmod +w /etc/nix/nix.conf
-sed -i 's/https:\/\/cache.nixos.org/https:\/\/mirrors.tuna.tsinghua.edu.cn\/nix-channels\/store/g' /etc/nix/nix.conf
+sed -i 's/https:\/\/cache.nixos.org/https:\/\/mirrors.tuna.tsinghua.edu.cn\/nix-channels\/store/\shttps:\/\/cache.nixos.org\/g' /etc/nix/nix.conf
