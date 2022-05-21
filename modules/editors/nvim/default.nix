da1138e3d04d181c818a84960c632e6557d8533c
zys864 <zyspage@gmail.com>
2022-05-21 23:35:19 +0800

    edit

--- modules/editors/nvim/default.nix
+++ modules/editors/nvim/default.nix
@@ -6,25 +6,6 @@
       enable = true;
       viAlias = true;
       vimAlias = true;
-
-      plugins = with pkgs.vimPlugins; [
-
-        # Syntax
-        vim-nix
-        vim-markdown
-
-        # Quality of life
-        vim-lastplace # Opens document where you left it
-        auto-pairs # Print double quotes/brackets/etc.
-        vim-gitgutter # See uncommitted changes of file :GitGutterEnable
-
-        # File Tree
-        nerdtree # File Manager - set in extraConfig to F6
-
-        lightline-vim # Info bar at bottom
-        indent-blankline-nvim # Indentation lines
-      ];
-    };
   };
 }
 
